51b62a83bb3448e751f57bf3b054e32e1f864d44
刘菊萍 <liujuping@meituan.com>
2017-04-14 15:41:22 +0800

    添加配置文件

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App',
+        factoryNum: 1,
+        machineNum: 6,
+        jobNum: 6,
+        plan: []
+      }
+    },
+    method: {
+      // 初始化染色体
+      initChromosome() {
+
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  img {
+    width: 100px;
+    float: left;
+    margin-right: 10px;
+  }
+
+</style>
